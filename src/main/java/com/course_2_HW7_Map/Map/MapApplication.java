e33a4f260cd6a601527210c54009712d46800468
Elena <elenela@yandex.ru>
2023-06-21 00:09:31 +0300

    Домашняя работа 7 (курс 2)

---
+++ src/main/java/com/course_2_HW7_Map/Map/MapApplication.java
@@ -0,0 +1,13 @@
+package com.course_2_HW7_Map.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
